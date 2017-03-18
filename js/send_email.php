55781d5542805489ad7c65b171617d095ec6b7c5
santiagoleira10@gmail.com <santiagoleira10@gmail.com>
2017-03-18 11:06:01 -0300

    página en el servidor

--- js/send_email.php
+++ js/send_email.php
@@ -1,19 +1,23 @@
 <?php
 if(isset($_POST["Nombre"]) && isset($_POST["Email"]) && isset($_POST["Mensaje"]) ){
-$to = "cabaniaslamorena@gmail.com";
-$subject = "WEB E-mail Contacto - CABANIAS LA MORENA";
-$contenido .= "Nombre: ".$_POST["Nombre"]."\n";
-$contenido .= "Email: ".$_POST["Email"]."\n";
-$contenido .= "Telefono: ".$_POST["Telefono"]."\n";
-$contenido .= "Contenido: ".$_POST["Mensaje"]."\n\n";
-$header = "From: no-reply@webcabaniaslamorena.com.ar\n";
-$header .= "Reply-To: ". $_POST["Email"]."\n";
-$header .= "Mime-Version: 1.0\n";
-$header .= "Content-Type: text/plain";
-if(mail($to, $subject, $contenido ,$header)){
-echo 1;
+
+	$to = "maniquiesargentina@hotmail.com";
+	$subject = "Email Contacto - Maniquies Argentina";
+	$contenido .= "Nombre: ".$_POST["Nombre"]."\n";
+	$contenido .= "Email: ".$_POST["Email"]."\n";
+	$contenido .= "Telefono: ".$_POST["Telefono"]."\n";
+	$contenido .= "Contenido: ".$_POST["Mensaje"]."\n\n";
+	$header = "From: no-reply@cabaniaslamorena.com.ar\n";
+	$header .= "Reply-To: ". $_POST["Email"]."\n";
+	$header .= "Mime-Version: 1.0\n";
+	$header .= "Content-Type: text/plain\n";
+	
+	if(mail($to, $subject, $contenido ,$header))
+		echo 1;
+	else
+		echo 0;
+
 }
 else
-echo 0;
-}
+	echo "Parámetros insuficientes"
 ?>
