a2926e34a173181060f7c98cd7e9eb1a869d8229
santiagoleira10@gmail.com <santiagoleira10@gmail.com>
2017-03-25 15:15:24 -0300

    cambios en envío de mail

--- js/send_email.php
+++ js/send_email.php
@@ -7,7 +7,7 @@ if(isset($_POST["Nombre"]) && isset($_POST["Email"]) && isset($_POST["Mensaje"])
 	$contenido .= "Email: ".$_POST["Email"]."\n";
 	$contenido .= "Telefono: ".$_POST["Telefono"]."\n";
 	$contenido .= "Contenido: ".$_POST["Mensaje"]."\n\n";
-	$header = "From: no-reply@cabaniaslamorena.com.ar\n";
+	$header = "From: no-reply@maniquiesargentina.com.ar\n";
 	$header .= "Reply-To: ". $_POST["Email"]."\n";
 	$header .= "Mime-Version: 1.0\n";
 	$header .= "Content-Type: text/plain\n";
